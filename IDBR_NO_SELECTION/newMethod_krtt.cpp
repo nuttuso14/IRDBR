df382a0c4cb5f5d65ac21a25731c535655d20920
nuttuso14 <32212880+nuttuso14@users.noreply.github.com>
2021-09-07 17:00:58 +0800

    add selection

--- IDBR_NO_SELECTION/newMethod_krtt.cpp
+++ IDBR_NO_SELECTION/newMethod_krtt.cpp
@@ -371,13 +371,22 @@ int main(int argc, char** argv)
     ww[5] = WiFi(6,ni[5],50,50,50,50,wifi_mean_time_connected,500,500,500,0,0);*/
 
         /* for mutiple bandwidth [no selection] */
-    cout << "======No selection unceratianty ======" <<endl;
+    /*cout << "======No selection unceratianty ======" <<endl;
     ww[0] = WiFi(1,ni[0],50,30,50,70,wifi_mean_time_connected,500,500,500,0.5,0.41);
     ww[1] = WiFi(2,ni[1],50,30,50,70,wifi_mean_time_connected,500,500,500,0.5,0.22);
     ww[2] = WiFi(3,ni[2],50,30,50,70,wifi_mean_time_connected,500,500,500,0.1,0.14);
     ww[3] = WiFi(4,ni[3],50,30,50,70,wifi_mean_time_connected,500,500,500,0.3,0.25);
     ww[4] = WiFi(5,ni[4],50,30,50,70,wifi_mean_time_connected,500,500,500,0.2,0.11);
-    ww[5] = WiFi(6,ni[5],50,30,50,70,wifi_mean_time_connected,500,500,500,0,0);
+    ww[5] = WiFi(6,ni[5],50,30,50,70,wifi_mean_time_connected,500,500,500,0,0);*/
+
+            /* for mutiple bandwidth [with selection] */
+    cout << "======No selection unceratianty [with selection]======" <<endl;
+    ww[0] = WiFi(1,ni[0],40,10,45,60.5,wifi_mean_time_connected,50,50,50,0.5,0.41);
+    ww[1] = WiFi(2,ni[1],50,1,45,100,wifi_mean_time_connected,100,100,100,0.5,0.22);
+    ww[2] = WiFi(3,ni[2],60,0.5,85,100,wifi_mean_time_connected,200,200,200,0.1,0.14);
+    ww[3] = WiFi(4,ni[3],40,2.5,35,80,wifi_mean_time_connected,15,15,15,0.3,0.25);
+    ww[4] = WiFi(5,ni[4],60,25,55,99,wifi_mean_time_connected,5,5,5,0.2,0.11);
+    ww[5] = WiFi(6,ni[5],5,0.5,6,8,wifi_mean_time_connected,500,500,500,0,0);
 
 
 
